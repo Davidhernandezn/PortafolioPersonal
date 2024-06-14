32b4e03f2c9d9367ef97afab67df1968b2b02f44
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-06-14 00:39:22 -0600

    Rutas cortas y estructura general para modulos y componentes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuHeaderComponent } from './components/menu-header/menu-header.component';
 
-
-
 @NgModule({
   declarations: [MenuHeaderComponent],
   imports: [
     CommonModule
+  ],
+  exports:[
+    MenuHeaderComponent
   ]
 })
 export class SharedModule { }
